0b7232a90704dff4fe28675f96336ef75c0afd82
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2018-11-09 22:49:59 +0600

    working on scroll top

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ $('#serach_button_icon').on('click', function () {
 $('#filter_button, .ok_filter, .cancel_filter').on('click', function () {
   $('.filter_box').toggleClass('active');
 })
+
+$main_content = $(".main_content");
+$sidebar_nav_content = $('.sidebar_nav_content');
+$main_content.on('scroll' () => {
+
+})
