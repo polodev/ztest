ed0c74af464ebb1cd59b738e657c0e42e629a33c
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2018-11-10 02:21:03 +0600

    changed in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,42 @@ $('#filter_button, .ok_filter, .cancel_filter').on('click', function () {
 $('#info_plus').on('click', function () {
   $('.sidebar_info_box').toggleClass('active');
 });
-
+var lastScrollTop = 0;
 $(window).scroll(function (event) {
-    var st = $(window).scrollTop();
-    var viewable_height = 400;
-    var windowHeight = $(window).height();
-    var main_content_height = $('.main_content').height();
-    var $svc = $('.sidebar_nav_content')
-    var sidebar_nav_content_height = $svc.height();
-    var stableHeight = sidebar_nav_content_height - viewable_height;
-    console.log('stableHeight', stableHeight)
-    console.log('sidebar_nav_content_height', sidebar_nav_content_height)
-    console.log('windowHeight', windowHeight)
-    console.log('st', st);
-    var footer_offset = $('footer').offset() - windowHeight;
-    console.log('footer offset', )
-    if (st < stableHeight  ) {
-      $svc.css('top', -st);
-    }
-    // if (st > main_content_height) {
-    //   $svc.css('top', -st);
-    // }
+  var sto = $(this).scrollTop();
+  var st = $(window).scrollTop();
+  var windowHeight = $(window).height();
+  var viewable_height = windowHeight/2;
+  var main_content_height = $('.main_content').height();
+  var $svc = $('.sidebar_nav_content')
+  var sidebar_nav_content_height = $svc.height();
+  var stableHeight = sidebar_nav_content_height - viewable_height;
+  var footer_offset = $('footer').offset() - windowHeight;
+
+   if (sto > lastScrollTop){
+      console.log('downscroll');
+      if (st < stableHeight  ) {
+        $svc.css('top', -st);
+      }
+      if (st > main_content_height ) {
+        $svc.css('top', -st);
+      }
+      console.log('st', st)
+      console.log('main_content_height', main_content_height)
+
+   } else {
+    console.log('upscroll');
+      // upscroll code
+      if (st > stableHeight && st < ((main_content_height+viewable_height) - windowHeight) ) {
+        // this is the place where I need to work upon 
+        $svc.css('top', -stableHeight);
+      }
+      if (st < stableHeight) {
+        $svc.css('top', - st);
+      }
+   }
+   lastScrollTop = sto;
+
+
+
 });
