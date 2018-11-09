7573736599a6787709a73f3dd889d215d462712a
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2018-11-09 21:26:38 +0600

    completed filters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 $('#serach_button_icon').on('click', function () {
   $('.input_box').toggleClass('active')
 })
+
+$('#filter_button, .ok_filter, .cancel_filter').on('click', function () {
+  $('.filter_box').toggleClass('active');
+})
