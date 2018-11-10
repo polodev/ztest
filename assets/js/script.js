82acf2bd84c4015d10e5a3650842c1ad4c3b2d56
Md. Shahinur Rahman <coredeveloper.2013@gmail.com>
2018-11-10 13:07:06 +0600

    changed in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,42 +8,40 @@ $('#filter_button, .ok_filter, .cancel_filter').on('click', function () {
 $('#info_plus').on('click', function () {
   $('.sidebar_info_box').toggleClass('active');
 });
+
+
 var lastScrollTop = 0;
+
+var windowHeight = $(window).height();
+var viewable_height = windowHeight/2;
+var main_content_height = $('.main_content').height();
+var $svc = $('.sidebar_nav_content')
+var sidebar_nav_content_height = $svc.height();
+var stableHeight = sidebar_nav_content_height - viewable_height;
+
 $(window).scroll(function (event) {
   var sto = $(this).scrollTop();
   var st = $(window).scrollTop();
-  var windowHeight = $(window).height();
-  var viewable_height = windowHeight/2;
-  var main_content_height = $('.main_content').height();
-  var $svc = $('.sidebar_nav_content')
-  var sidebar_nav_content_height = $svc.height();
-  var stableHeight = sidebar_nav_content_height - viewable_height;
-  var footer_offset = $('footer').offset() - windowHeight;
 
-   if (sto > lastScrollTop){
-      console.log('downscroll');
-      if (st < stableHeight  ) {
-        $svc.css('top', -st);
-      }
-      if (st > main_content_height ) {
-        $svc.css('top', -st);
-      }
-      console.log('st', st)
-      console.log('main_content_height', main_content_height)
-
-   } else {
+ if (sto > lastScrollTop){
+    stableHeight = sidebar_nav_content_height - viewable_height;
+    console.log('downscroll');
+    if (st < stableHeight  ) {
+      $svc.css('top', -st);
+    }
+    if (st > main_content_height ) {
+      $svc.css('top', -st);
+    }
+    console.log('st', st)
+    console.log('main_content_height', main_content_height)
+ } else {
     console.log('upscroll');
-      // upscroll code
-      if (st > stableHeight && st < ((main_content_height+viewable_height) - windowHeight) ) {
-        // this is the place where I need to work upon 
-        $svc.css('top', -stableHeight);
-      }
-      if (st < stableHeight) {
-        $svc.css('top', - st);
-      }
+    console.log('stableHeight', stableHeight);
+    console.log('st', stableHeight);
+    stableHeight = (st > 0 && stableHeight > 0) ? (stableHeight - 4) : 0;
+    $svc.css('top', -( (st > 0 && stableHeight > 0) ? stableHeight-- : 0));
+
    }
    lastScrollTop = sto;
-
-
 
 });
